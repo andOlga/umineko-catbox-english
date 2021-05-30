6ffcf8102f23971a08811daf5f05680af626bd2b
ooa113y <13thSlayer@gmail.com>
2021-05-31 00:09:54 +0300

    Update buildscript to match mod dir paths

--- build.sh
+++ build.sh
@@ -3,4 +3,7 @@ rm patch.rom
 cd repack/rom-repack
 ruby kal_real.rb ../../script.rb ../../ass ../../patch.rom ../../patch.snr 6 19 88
 cd ../..
-rm patch.snr
+mkdir -p english/romfs/
+mv patch.rom english/romfs/patch.rom
+zip -r patch.zip english
+rm patch.snr english -r
