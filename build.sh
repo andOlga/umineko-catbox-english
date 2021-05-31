7d4fb37fc3fdc86229ac1b1c2d4afad38f4c06d4
ooa113y <13thSlayer@gmail.com>
2021-05-31 03:07:58 +0300

    Add exefs

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd repack/rom-repack
-ruby kal_real.rb ../../script.rb ../../ass ../../patch.rom ../../patch.snr 6 19 88
+ruby kal_real.rb ../../script.rb ../../romfs ../../patch.rom ../../patch.snr 6 19 88
 cd ../..
 mkdir -p english/romfs/
 mv patch.rom english/romfs/patch.rom
+cp -r exefs english/exefs
 zip -r patch.zip english
-rm patch.snr english -r
