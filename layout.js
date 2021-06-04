b04675f8547d6a042465e21cd63ee49852e25816
ooa113y <13thSlayer@gmail.com>
2021-06-04 10:40:39 +0300

    Improve layouter

--- layout.js
+++ layout.js
@@ -3,9 +3,8 @@ const startLine = 18472
 const lines = fs.readFileSync('script.rb', 'utf-8').split('\n')
 for (let i = startLine; i < lines.length; i++) {
   if (
-    lines[i].startsWith('s.ins 0x86') && 
-    !lines[i].includes('s.layout') &&
-    !lines[i].match(/[^\x00-\x7F]/) // eslint-disable-line no-control-regex
+    lines[i].startsWith('s.ins 0x86') &&
+    !lines[i].includes('s.layout')
   ) {
     lines[i] = lines[i].replace(/('.+')$/, 's.layout($1)')
   }
