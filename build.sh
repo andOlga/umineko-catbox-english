1dd26b7da21685f52cdcfd4ad10751e1f20c47a9
ooa113y <13thSlayer@gmail.com>
2021-06-05 16:06:09 +0300

    Add ips patch builder for Bern's game

--- build.sh
+++ build.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+set -e
 cd repack/rom-repack
 ruby kal_real.rb ../../script.rb ../../romfs ../../patch.rom ../../patch.snr 6 19 88
 cd ../..
+python3 build_bgame.py
 MODBASE=mods/contents/01006a300ba2c000/english/
 mkdir -p $MODBASE/romfs/
 mv patch.rom $MODBASE/romfs/patch.rom
 cp -r exefs $MODBASE/exefs
+mkdir -p mods/exefs_patches/umineko/
+mv *.ips mods/exefs_patches/umineko/
 zip -r patch.zip mods
 rm -r mods patch.snr
 # This is a script I use to copy the mod to my emulator right away, it's not in the repo
---
+++ build_bgame.py
@@ -0,0 +1,30 @@
+import numpy
+import sys
+
+with open("exefs_texts.txt", 'r') as f:
+    Offsets = [line.split("\t", -1)[0] for line in f]
+    f.seek(0,0)
+    Texts = [line.split("\t", -1)[1] for line in f]
+    f.seek(0,0)
+    TextsCheck = [line.strip("\r\n").strip("\n").split("\t", -1)[2] for line in f]
+
+print("Validating...")
+
+for i in range(0, len(Offsets)):
+    if (len(bytes(Texts[i].encode("UTF-8"))) > len(bytes(TextsCheck[i].encode("UTF-8")))):
+        print("Incorrect size. Line: %d, Max allowed: %d B, detected: %d B" % (i+1, len(bytes(TextsCheck[i].encode("UTF-8"))), len(bytes(Texts[i].encode("UTF-8")))))
+        input("Press Enter to continue...")
+        sys.exit()
+
+print("Writing patch...")
+
+with open("7616F8963DACCD70E20FF3904E13367F96F2D9B3000000000000000000000000.ips", "wb") as f:
+    f.write(b"IPS32")
+    for i in range(0, len(Offsets)):
+        f.write(numpy.uint32(int(Offsets[i], 16)+0x100).byteswap())
+        f.write(numpy.uint16(len(Texts[i].encode("UTF-8"))+1).byteswap())
+        f.write(bytes(Texts[i].encode("UTF-8")))
+        f.write(numpy.uint8(0))
+    f.write(b"EEOF")
+
+print("Patch has been created.")
