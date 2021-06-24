4aa4c8c3280416396f9d21e5a5e43bd902fb4320
ooa113y <13thSlayer@gmail.com>
2021-06-25 02:40:02 +0300

    Add non-dialogue replacement mode

--- replacer.js
+++ replacer.js
@@ -7,10 +7,11 @@ if (!fs.existsSync(scriptJp) || !fs.existsSync(scriptEn)) break
 
 const linesJp = fs.readFileSync(scriptJp, 'utf-8').split('\n')
 const linesEn = fs.readFileSync(scriptEn, 'utf-8').split('\n')
-
+const replaceDialogue = true
+const lineFuns = replaceDialogue ? [x => x + '@', x => x + "'", x => x.trim() + '@', x => x.trim() + "'"] : [x => x]
 for (let i = 0; i < linesJp.length; i++) {
   if (linesEn[i]) {
-    for (const fun of [x => x + '@', x => x + "'", x => x.trim() + '@', x => x.trim() + "'"]) {
+    for (const fun of lineFuns) {
       const tmpScript = targetScript.replace(fun(linesJp[i]), fun(linesEn[i]))
       if (tmpScript !== targetScript) {
         targetScript = tmpScript
