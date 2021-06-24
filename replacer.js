4ebf6c825de6cb6e7a91ba45637369bba3608506
ooa113y <13thSlayer@gmail.com>
2021-06-25 02:36:25 +0300

    Update replacer to work on single chapters

--- replacer.js
+++ replacer.js
@@ -1,37 +1,22 @@
 const fs = require('fs')
-const startLine = 18473 - 1
-let targetScript = fs.readFileSync('script.rb', 'utf-8').split('\n')
-let output = targetScript.slice(0, startLine).join('\n') + '\n'
-targetScript = targetScript.slice(startLine).join('\n')
-const scriptBase = 'script_plain/umi'
-for (let ep = 1; ep <= 8; ep++) {
-  console.log(`Processing Episode ${ep}`)
-  for (let chapter = 0; chapter <= 100; chapter++) {
-    const scriptJp = `${scriptBase}${ep}_${chapter}_jp.txt`
-    const scriptEn = `${scriptBase}${ep}_${chapter}.txt`
-    if (!fs.existsSync(scriptJp)) break
+let targetScript = fs.readFileSync('replace_script.rb', 'utf-8')
+const scriptBase = `script_plain/${process.argv[2]}`
+const scriptJp = `${scriptBase}_jp.txt`
+const scriptEn = `${scriptBase}.txt`
+if (!fs.existsSync(scriptJp) || !fs.existsSync(scriptEn)) break
 
-    const linesJp = fs.readFileSync(scriptJp, 'utf-8').split('\n')
-    const linesEn = fs.readFileSync(scriptEn, 'utf-8').split('\n')
+const linesJp = fs.readFileSync(scriptJp, 'utf-8').split('\n')
+const linesEn = fs.readFileSync(scriptEn, 'utf-8').split('\n')
 
-    let chapterScript = targetScript.split('\n')
-    const lineIdx = chapterScript.findIndex(x => x.startsWith('s.ins 0xa0, byte(1), ')) + 1
-    chapterScript = chapterScript.slice(0, lineIdx)
-    chapterScript = chapterScript.join('\n')
-    for (let i = 0; i < linesJp.length; i++) {
-      if (linesEn[i]) {
-        for (const fun of [x => x + '@', x => x + "'", x => x.trim() + '@', x => x.trim() + "'"]) {
-          const tmpScript = chapterScript.replace(fun(linesJp[i]), fun(linesEn[i]))
-          if (tmpScript !== chapterScript) {
-            chapterScript = tmpScript
-            break
-          }
-        }
+for (let i = 0; i < linesJp.length; i++) {
+  if (linesEn[i]) {
+    for (const fun of [x => x + '@', x => x + "'", x => x.trim() + '@', x => x.trim() + "'"]) {
+      const tmpScript = targetScript.replace(fun(linesJp[i]), fun(linesEn[i]))
+      if (tmpScript !== targetScript) {
+        targetScript = tmpScript
+        break
       }
     }
-
-    output += chapterScript + '\n'
-    targetScript = targetScript.split('\n').slice(lineIdx).join('\n')
   }
 }
-fs.writeFileSync('script.rb', output + '\n' + targetScript, 'utf-8')
+fs.writeFileSync('replace_script_out.rb', output + '\n' + targetScript, 'utf-8')
